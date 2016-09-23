281d34bb9b529fc57eba1664ebaf21bcabb2192d
mmoradeveloper <marc@experttrades.com>
2016-09-23 16:28:44 +0000

    page

--- src/AppBundle/Entity/Page/Page.php
+++ src/AppBundle/Entity/Page/Page.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping\OrderBy;
+use AppBundle\Entity\Page\Item\Item;
 
 
 
